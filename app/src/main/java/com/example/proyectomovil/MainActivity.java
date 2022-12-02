0879ca2179fb66da8d66401ac5e9fcff7fc4c51e
Luis Angel <49156466+Adero210@users.noreply.github.com>
2022-12-01 23:57:52 -0600

    Login Activity

--- app/src/main/java/com/example/proyectomovil/MainActivity.java
+++ app/src/main/java/com/example/proyectomovil/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    String REGISTRADO = "registrado";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i;
+                if(nuevoUsuario()){
+                    i = new Intent(MainActivity.this, MenuPrincipal.class);
+                }else{
+                    i = new Intent(MainActivity.this,ActivityLogin.class);
+                }
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,1000);
+    }
+
+    private boolean nuevoUsuario() {
+        SharedPreferences preferences = this.getSharedPreferences("user.dat",MODE_PRIVATE);
+        return preferences.getBoolean(REGISTRADO,false);
     }
 }
