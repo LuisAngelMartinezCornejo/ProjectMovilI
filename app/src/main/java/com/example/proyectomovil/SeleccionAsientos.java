72e9fca373c9c337b541f08c50b938cb7e960763
Luis Angel <49156466+Adero210@users.noreply.github.com>
2022-12-03 10:31:40 -0600

    Ultimas activities

--- app/src/main/java/com/example/proyectomovil/SeleccionAsientos.java
+++ app/src/main/java/com/example/proyectomovil/SeleccionAsientos.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class
-MisViajes extends AppCompatActivity {
+public class SeleccionAsientos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mis_viajes);
+        setContentView(R.layout.activity_seleccion_asientos);
     }
 }
