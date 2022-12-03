f26a1ef8fb9feac071d1d4292b1ce39087459452
Eric Martinez <ericmage2001@gmail.com>
2022-12-03 12:00:22 -0600

    Funcionalidad de MiPerfil y EditarPerfil

--- app/src/main/java/com/example/proyectomovil/EditarPerfil.java
+++ app/src/main/java/com/example/proyectomovil/EditarPerfil.java
@@ -2,13 +2,50 @@ package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
 
 public class EditarPerfil extends AppCompatActivity {
+
+    Usuario user;
+
+    TextView saludo;
+    EditText edtNombre, edtCorreo, edtTelefono, edtDireccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
+
+        user = (Usuario) getIntent().getSerializableExtra("user");
+
+        saludo = (TextView) findViewById(R.id.txtSaludoPerfil2);
+        edtNombre = (EditText) findViewById(R.id.edtNombre);
+        edtCorreo = (EditText) findViewById(R.id.edtCorreo);
+        edtTelefono = (EditText) findViewById(R.id.edtTelefono);
+        edtDireccion = (EditText) findViewById(R.id.edtDireccion);
+
+        saludo.setText(MessageFormat.format("¡Hola {0}!", user.getNombre()));
+        edtNombre.setText(user.getNombre());
+        edtCorreo.setText(user.getCorreo());
+        edtTelefono.setText(user.getCorreo());
+        edtDireccion.setText(user.getDireccion());
+    }
+
+    public void guardarInformacion(View view){
+        user.setNombre(edtNombre.getText().toString());
+        user.setCorreo(edtCorreo.getText().toString());
+        user.setTelefono(Integer.parseInt(edtTelefono.getText().toString()));
+        user.setDireccion(edtDireccion.getText().toString());
+
+        Intent intent = new Intent(this, MiPerfil.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+        finish();
     }
 }
