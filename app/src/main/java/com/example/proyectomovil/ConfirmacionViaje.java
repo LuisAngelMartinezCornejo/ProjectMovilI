00a164f29753cab65933831fdca2adcb5dada221
Eric Martinez <ericmage2001@gmail.com>
2022-12-03 14:14:31 -0600

    Cambio ConfirmacionViaje

--- app/src/main/java/com/example/proyectomovil/ConfirmacionViaje.java
+++ app/src/main/java/com/example/proyectomovil/ConfirmacionViaje.java
@@ -2,13 +2,50 @@ package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ConfirmacionViaje extends AppCompatActivity {
+
+    Usuario user;
+    Viaje viaje;
+
+    TextView idViaje, destino, transporte, fecha, dias, asientos, reservacionNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacion_viaje);
+
+        user = (Usuario) getIntent().getSerializableExtra("user");
+        viaje = (Viaje) getIntent().getSerializableExtra("viaje");
+
+        idViaje = (TextView) findViewById(R.id.txtIDViaje);
+        destino = (TextView) findViewById(R.id.txtDestino);
+        transporte = (TextView) findViewById(R.id.txtTransporte);
+        fecha = (TextView) findViewById(R.id.txtFecha);
+        dias = (TextView) findViewById(R.id.txtDias);
+        asientos = (TextView) findViewById(R.id.txtAsientos);
+        reservacionNombre = (TextView) findViewById(R.id.txtReservacion);
+
+        idViaje.setText(viaje.getId());
+        destino.setText(viaje.getLugar().getCiudad());
+        transporte.setText(viaje.getTransporte().getTipo());
+        fecha.setText(viaje.getFecha().toString());
+        dias.setText(viaje.getDiasEstancia());
+        asientos.setText(viaje.getAsientos());
+        reservacionNombre.setText(viaje.getNombreReserva());
+    }
+
+    public void entendido(View view){
+        user.viajesUsuario[user.contadorViajes] = viaje;
+        user.contadorViajes++;
+
+        Intent intent = new Intent(this, MenuPrincipal.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+        finish();
     }
 }
