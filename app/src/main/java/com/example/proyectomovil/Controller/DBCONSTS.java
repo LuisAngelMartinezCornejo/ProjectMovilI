92fcee5d9079e25d678d5aa2e3ebc0722a56a16f
Mauricio Farfan <maufar402@gmail.com>
2023-06-09 23:20:11 -0600

    Agregando todas las rutas de la API y limpiando un poco usando packages.

---
+++ app/src/main/java/com/example/proyectomovil/Controller/DBCONSTS.java
@@ -0,0 +1,8 @@
+package com.example.proyectomovil;
+
+public class DBCONSTS {
+
+    public static String baseRoute = BuildConfig.API_ROUTE;
+
+    public static String URL_AUTH = baseRoute + "/api/users/auth/";
+}
---
+++ app/src/main/java/com/example/proyectomovil/interfaces/VolleyCallback.java
@@ -0,0 +1,2 @@
+package com.example.proyectomovil.interfaces;public interface VolleyCallback {
+}
